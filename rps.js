00a8367e3a19157af615002a36a55098fbaf3751
Alvin Cousins <alvin.cousins@googlemail.com>
2021-02-16 19:01:48 +0000

    Added basic UI

--- rps.js
+++ rps.js
@@ -1,8 +1,5 @@
 
-
-
-
-
+// Returns rock, paper or scissors at random.
 function computerPlay() {
     var i;
     switch (Math.floor(Math.random() * 3)) {
@@ -19,66 +16,111 @@ function computerPlay() {
     return i;
 }
 
+// Returns win, lose or tie.
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock"){
         if (computerSelection == "rock") {
+            console.log("Rock cancels out rock.");
             return "tie";
         }
         else if (computerSelection == "paper") {
+            console.log("Paper beats rock.")
             return "lose";
         }
-        else {
+        else if (computerSelection == "scissors") {
+            console.log("Rock beats scissors.")
             return "win";
-        }
-    }
-    if (playerSelection == "paper"){
-        if (computerSelection == "rock") {
-            return "win";
-        }
-        else if (computerSelection == "paper") {
-            return "tie";
         }
         else {
             return "lose";
         }
     }
-    if (playerSelection == "scissors"){
+    else if (playerSelection == "paper"){
         if (computerSelection == "rock") {
+            console.log("Paper beats rock.")
+            return "win";
+        }
+        else if (computerSelection == "paper") {
+            console.log("Paper cancels out paper.")
+            return "tie";
+        }
+        else if (computerSelection == "scissors") {
+            console.log("Scissors beats paper.")
+            return "lose";
+        }
+        else {
+            return "lose";
+        }
+    }
+    else if (playerSelection == "scissors"){
+        if (computerSelection == "rock") {
+            console.log("Rock beats scissors.")
             return "lose";
         }
         else if (computerSelection == "paper") {
+            console.log("Scissors beats paper.")
             return "win";
         }
-        else {
+        else if (computerSelection == "scissors") {
+            console.log("Scissors cancels out scissors.")
             return "tie";
         }
+        else {
+            return "lose";
+        }
+    
+    }
+    else {
+        console.log("Player forfeits.");
+        return "lose";
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-
-
-//console.log(playRound(playerSelection, computerSelection));
-
-function prompt() {
-    var prompt = require('prompt');
-    var humanSelection;
-    prompt.start();
-        prompt.get('selection', function (err, result) {
-            //console.log('Human: ' + result.selection);
-            humanSelection = result.selection;
-        });
-        return humanSelection;
-}
 
 function game() {
-    var rounds = 3;
+    var rounds = 1;
     var playerSelection;
+    var wins = 0;
 
-    var humanSelection = prompt();
-    console.log("Human selects", humanSelection);
+    for (var i = 0; i < rounds; i++) {
+        var playerSelection = prompt(`Round ${i + 1}: \nRock, paper, scissors?`);
+        //console.log("Human selects", playerSelection);
+    
+        var computerSelection = computerPlay();
+        console.log(`Human selects ${playerSelection}. Computer selects ${computerSelection}`)
+
+        var result = playRound(playerSelection, computerSelection)
+            if (result == "win") {
+                wins++;
+                console.log("Win")
+            }
+            else if (result == "lose") {
+                console.log("Lose")
+            }
+            else {
+                wins += 0.5;
+                console.log("Tie")
+            }
+
+        console.log(`Current score\nHuman: ${wins}\nComputer: ${(i + 1) - wins}`)
+            
+        }
+
+    //console.log(playRound(playerSelection, computerSelection));
+    
+    if (wins == 1) {
+        console.log(`Human loses at 1 game to 5!`)
+    }
+    else if (wins > 2.5) {
+        console.log(`Human wins at ${wins} games to ${rounds - wins}`)
+    }
+    else if (wins == 2.5) {
+        console.log(`Draw with 2.5 wins.`)
+    }
+    else {
+        console.log(`Human loses at ${wins} games to ${rounds - wins}`)
+    }
 
 }
 
-game()
+//game()
